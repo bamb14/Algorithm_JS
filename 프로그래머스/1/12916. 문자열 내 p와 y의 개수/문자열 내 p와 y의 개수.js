5db355d524e58ab8af04d65949208ff8617f2caa
Miso Kim <118671229+bamb14@users.noreply.github.com>
2024-07-03 19:56:40 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,13 @@
+function solution(s){
+    var answer = s.toLowerCase();
+    const len=s.length;
+    let p_num=0;
+    let y_num=0;
+    for(let i=0; i<len; i++){
+        if(answer[i]==='p') p_num++;
+        else if(answer[i]==='y') y_num++;
+    }
+    if(p_num===0&&y_num===0) return true;
+    else if(p_num===y_num) return true;
+    else return false;
+}
