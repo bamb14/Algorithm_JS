2cb531146808bcf980125978ff504e47e327ca63
Miso Kim <118671229+bamb14@users.noreply.github.com>
2024-07-09 11:35:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,18 +1,18 @@
 function solution(s) {
     var answer='';
     var en='';
-    let NumEN={
-       'zero': '0',
-         'one':'1',
-        'two' : '2',
-         'three': '3',
-         'four':'4',
-         'five':'5',
-        'six':'6',
-        'seven':'7',
-         'eight':'8',
-      'nine' :'9'
-    }
+    let NumEN = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    };
     const len=s.length;
     for(let i=0; i<len; i++){
         if(!isNaN(s[i])){
