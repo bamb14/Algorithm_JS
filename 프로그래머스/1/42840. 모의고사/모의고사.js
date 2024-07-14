0659450a889152eaa0bb7ea1f232f2c59418ec15
Miso Kim <118671229+bamb14@users.noreply.github.com>
2024-07-14 16:36:58 +0900

    [level 1] Title: 모의고사, Time: 3.26 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -4,20 +4,20 @@ function solution(answers) {
     const second = [2, 1, 2, 3, 2, 4, 2, 5];
     const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-    const scores = [0, 0, 0];  // 각각의 학생이 맞힌 문제 수를 저장
+    const scores = [0, 0, 0];
 
     for (let i = 0; i < len; i++) {
-        if (answers[i] === first[i % first.length]) scores[0]++;
-        if (answers[i] === second[i % second.length]) scores[1]++;
-        if (answers[i] === third[i % third.length]) scores[2]++;
+        if (answers[i] === first[i % 5]) scores[0]++;
+        if (answers[i] === second[i % 8]) scores[1]++;
+        if (answers[i] === third[i % 10]) scores[2]++;
     }
 
-    const maxScore = Math.max(...scores);  // 가장 높은 점수 찾기
+    const maxScore = Math.max(...scores);
     const result = [];
 
     for (let i = 0; i < 3; i++) {
         if (scores[i] === maxScore) {
-            result.push(i + 1);  // 학생 번호는 1부터 시작하므로 i+1
+            result.push(i + 1);  
         }
     }
 
