312023cc7ebf5b85ea1c3afa35ab1c41a0ffbd3b
Miso Kim <118671229+bamb14@users.noreply.github.com>
2024-09-01 17:42:16 +0900

    [level 1] Title: 과일 장수, Time: 215.01 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,8 @@
+function solution(k, m, score) {
+    score=score.sort((a,b)=>b-a);
+    let result=0;
+    for(let i=m-1; i<score.length; i+=m){
+        result+=(score[i]*m);   
+    }
+    return result;
+}
