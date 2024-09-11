0fb2aa7b416c23cc5e1825475ce2a1dcaefce67e
Miso Kim <118671229+bamb14@users.noreply.github.com>
2024-09-12 01:20:37 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    s=s.split(" ");
+    for(i in s){
+         if (s[i] !== '') {
+            const upper=s[i][0].toUpperCase();
+            const lower=s[i].slice(1,).toLowerCase();
+            s[i]=upper+lower;
+         }
+    }
+    return s.join(' ');
+}
