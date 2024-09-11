e9dcca5dc9cf716d1791ed8d29084d7fb9cf0c2b
Miso Kim <118671229+bamb14@users.noreply.github.com>
2024-09-12 00:57:10 +0900

    [level 2] Title: 올바른 괄호, Time: 24.54 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let left_cnt = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            left_cnt++;
+        } else if (s[i] === ')') {
+            left_cnt--;
+        }
+        if (left_cnt < 0) { 
+            return false; // 닫힌 괄호가 열린 괄호보다 먼저 나오는 경우
+        }
+    }
+
+    return left_cnt === 0; // 모든 괄호가 짝이 맞는 경우
+}
