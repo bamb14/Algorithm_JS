aa12bc805e644f566d279fe5a851c0ad12545ca3
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-01-13 15:34:57 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 9972 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -1,22 +1,32 @@
 const fs = require("fs");
 let a = fs.readFileSync(0).toString().trim().split('\n');
 
-list_0=[1,0]
-list_1=[0,1]
 
-function solution(n){
-  const list_0 = [1, 0];
-  const list_1 = [0, 1];
 
-  for (let i = 2; i <= n; i++) {
-    list_0.push(list_0[i - 1] + list_0[i - 2]);
-    list_1.push(list_1[i - 1] + list_1[i - 2]);
+
+function solution_0(n){
+  if(n===0) return 1;
+  else if(n===1) return 0;
+  else{
+      list_0=[1,0]
+    for(let i=2; i<=n; i++){
+      list_0.push(list_0[i-1]+list_0[i-2])
+    }
+    return list_0[n]
   }
-
-  return [list_0[n], list_1[n]];
+}
+function solution_1(n){
+  if(n===0) return 0;
+  else if(n===1) return 1;
+  else{
+     list_1=[0,1]
+    for(let i=2; i<=n; i++){
+      list_1.push(list_1[i-1]+list_1[i-2])
+    }
+    return list_1[n]
+  }
 }
 
 for(let i=1; i<=a[0]; i++){
-  const [count_0, count_1] = solution(a[i]);
-  console.log(count_0, count_1);
+  console.log(solution_0(a[i]), solution_1(a[i]));
 }
