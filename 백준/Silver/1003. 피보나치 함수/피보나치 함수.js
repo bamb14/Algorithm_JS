dc6490f77e9d665d89b3a49b43fb0721f4713c99
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-01-13 15:33:03 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 9988 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -1,30 +1,22 @@
 const fs = require("fs");
 let a = fs.readFileSync(0).toString().trim().split('\n');
 
-function solution_0(n) {
-    if (n === 0) return 1; // F(0) = 1
-    if (n === 1) return 0; // F(1) = 0
-    else {
-        let array_0 = [1, 0]; // F(0) = 1, F(1) = 0
-        for (let i = 2; i <= n; i++) { // n까지 반복
-            array_0.push(array_0[i - 2] + array_0[i - 1]);
-        }
-        return array_0[n]; // n번째 항을 반환
-    }
+list_0=[1,0]
+list_1=[0,1]
+
+function solution(n){
+  const list_0 = [1, 0];
+  const list_1 = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    list_0.push(list_0[i - 1] + list_0[i - 2]);
+    list_1.push(list_1[i - 1] + list_1[i - 2]);
+  }
+
+  return [list_0[n], list_1[n]];
 }
 
-function solution_1(n) {
-    if (n === 0) return 0; // F(0) = 0
-    if (n === 1) return 1; // F(1) = 1
-    else {
-        let array_1 = [0, 1]; // F(0) = 0, F(1) = 1
-        for (let i = 2; i <= n; i++) { // n까지 반복
-            array_1.push(array_1[i - 2] + array_1[i - 1]);
-        }
-        return array_1[n]; // n번째 항을 반환
-    }
-}
-
-for (let i = 1; i <= parseInt(a[0]); i++) { // 1부터 a[0]까지 반복
-    console.log(solution_0(parseInt(a[i])), solution_1(parseInt(a[i])));
+for(let i=1; i<=a[0]; i++){
+  const [count_0, count_1] = solution(a[i]);
+  console.log(count_0, count_1);
 }
