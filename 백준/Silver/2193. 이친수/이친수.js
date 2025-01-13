bffa84934615ef3499584663cf84c2663de2ada2
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-01-13 11:33:27 +0900

    [Silver III] Title: 이친수, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.js
+++ 백준/Silver/2193. 이친수/이친수.js
@@ -1,24 +1,18 @@
 const fs = require("fs");
 
-// 입력 받기
 let a = fs.readFileSync(0).toString().trim();
 
-// BigInt로 변환
 let bitInt = BigInt(a);
 
-// 피보나치 수열 계산
 let list = [];
 list[1] = 1n;
 list[2] = 1n;
 
 function solution_dfs(n) {
-  // 피보나치 수열을 1부터 n까지 계산
   for (let i = 3; i <= n; i++) {
     list[i] = list[i - 1] + list[i - 2];
   }
-  // n번째 피보나치 수 출력
   console.log(list[n].toString());
 }
 
-// 피보나치 수열 계산 실행
 solution_dfs(bitInt);
