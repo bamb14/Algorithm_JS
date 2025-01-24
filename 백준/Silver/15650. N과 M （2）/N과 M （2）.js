81fbf6f6239ce0ea687f8e64707224daa42a9cd2
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-01-24 19:36:42 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+let a = fs.readFileSync(0).toString().trim().split('\n');
+const [n,m] = a[0].split(" ").map(Number);
+let visited=Array.from({length: n}).fill(0)
+let answer=[]
+
+solution(0,0)
+
+function solution(depth, idx){
+    if(depth===m){
+        const result=answer.join(' ')
+        console.log(result)
+        return;
+    }
+    
+    for(let i=idx+1; i<=n; i++){
+        if(!visited[i]){
+            visited[i]=1;
+            answer.push(i)
+            solution(depth+1,i);
+            answer.pop();
+            visited[i]=0;
+        }
+    }
+}
