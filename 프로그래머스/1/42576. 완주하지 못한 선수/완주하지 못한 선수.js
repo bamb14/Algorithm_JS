f91d833832377b3b3a6375a01d4a1fee9d9db49a
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-02-05 10:10:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.40 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,26 +1,19 @@
 function solution(participant, completion) {
-    const map = new Map();
-
-    // 참가자 명단을 순회하면서 각 참가자의 이름과 등장 횟수를 기록
-    for (const person of participant) {
-        if (map.has(person)) {
-            map.set(person, map.get(person) + 1);
+    const map=new Map()
+    
+    for(let name of participant){
+        if (map.has(name)) {
+            map.set(name, map.get(name) + 1);
         } else {
-            map.set(person, 1);
+            map.set(name, 1);
         }
     }
-
-    // 완주자 명단을 순회하면서 각 참가자의 등장 횟수를 차감
-    for (const person of completion) {
-        if (map.has(person)) {
-            map.set(person, map.get(person) - 1);
-        }
+    
+    for(let name of completion){
+        map.set(name, map.get(name) - 1);
     }
-
-    // 등장 횟수가 1인 참가자가 완주하지 못한 사람
-    for (const [key, value] of map) {
-        if (value > 0) {
-            return key;
-        }
+    
+    for(const [key, value] of map){
+        if(value>0) return key
     }
 }
