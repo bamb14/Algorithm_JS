b91ee0f145dbcca691c2a86b9f27ae18a8a644ab
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-02-05 11:16:42 +0900

    [level 3] Title: 입국심사, Time: 32.11 ms, Memory: 42.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,31 @@
+function solution(n, times) {
+    let total=0;
+    let start=0;
+    let end= 1000000000 * 100000
+   
+    while (start <= end) {
+        middle = Math.floor((start + end) / 2);
+
+        if (check(middle, n, times)) { // 범위를 더 줄여
+            total = middle;
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+    }
+    return total;
+}
+
+function check(curTime, n, times){
+    let sum=0;
+    
+    for (const time of times) {
+        sum += Math.floor(curTime / time);
+    }
+
+    if (sum < n) { // 주어진 인원수를 충족하지 못하는 경우
+        return false;
+    }
+    
+    return true; // 주어진 인원수를 충족하는 경우
+}
