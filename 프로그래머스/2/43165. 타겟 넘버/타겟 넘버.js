6f4c2e15216b68b333b307c1f0bda6f3e1fd67c6
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-02-05 20:53:53 +0900

    [level 2] Title: 타겟 넘버, Time: 30.59 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,32 +1,23 @@
 function solution(numbers, target) {
     let answer = 0;
-
-    const dfs = (numbers,idx,curX) =>{
-        if(idx == numbers.length && curX == target){ //numbers 내 모든 수를 돌았고, 타겟 넘버에 맞춤
+    const length=numbers.length;
+    
+    const dfs = (idx,curX) =>{
+        if(idx == length && curX == target){
+            //numbers 내 모든 수를 돌았고, 타겟 넘버에 맞춤
             answer += 1;
             return
         }
-        else if(idx == numbers.length && curX != target){ //numbers 내 모든 수를 돌았지만 타겟넘버에 맞추지 못함
+        else if(idx == length && curX != target){
+            //numbers 내 모든 수를 돌았지만 타겟넘버에 맞추지 못함
             return;
         }
         //모든 수에 대해 +/- 재귀함수로 돌기
-        dfs(numbers, idx + 1, curX - numbers[idx])
-        dfs(numbers, idx + 1, curX + numbers[idx]);
+        dfs(idx + 1, curX - numbers[idx]);
+        dfs(idx + 1, curX + numbers[idx]);
     }
 
-    dfs(numbers,0,0); //재귀함수 시작
+    dfs(0,0); //재귀함수 시작
 
     return answer;
-}
-function dfs(numbers,idx,curX){
-    if(idx == numbers.length && curX == target){ //numbers 내 모든 수를 돌았고, 타겟 넘버에 맞춤
-            answer += 1;
-            return
-        }
-        else if(idx == numbers.length && curX != target){ //numbers 내 모든 수를 돌았지만 타겟넘버에 맞추지 못함
-            return;
-        }
-        //모든 수에 대해 +/- 재귀함수로 돌기
-        dfs(numbers, idx + 1, curX - numbers[idx])
-        dfs(numbers, idx + 1, curX + numbers[idx]);
 }
