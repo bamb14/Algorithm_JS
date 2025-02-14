9361dc74846a2206925033c03b85e85907ec14a0
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-02-14 18:11:17 +0900

    [Silver I] Title: 지름길, Time: 168 ms, Memory: 11992 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.js
@@ -0,0 +1,22 @@
+const fs=require("fs")
+let input=fs.readFileSync(0).toString().trim().split('\n')
+
+const [n,d]=input.shift().split(' ').map(Number)
+const list=input.map(str=>str.split(' ').map(Number))
+list.sort((a, b) => a[1] - b[1] || a[0] - b[0])
+
+let dp=new Array(d+1).fill(Infinity)
+dp[0]=0
+let current=1;
+
+for (let i = 0; i <= d; i++) {
+  if (i > 0) dp[i] = Math.min(dp[i], dp[i - 1] + 1);
+
+  for (const [start, end, dist] of list) {
+    if (start === i && end <= d) {
+      dp[end] = Math.min(dp[end], dp[start] + dist);
+    }
+  }
+}
+
+console.log(dp[d])
