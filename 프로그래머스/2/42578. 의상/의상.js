e6dbe5645605ecae6979900f4f0a43dacd264538
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-02-14 23:17:51 +0900

    [level 2] Title: 의상, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,16 @@
+function solution(clothes) {
+    let answer=1;
+    const map=new Map()
+    for(const [item, category] of clothes){
+        if(map.has(category)) map.set(category, map.get(category)+1)
+        else map.set(category, 1)
+    }
+    const keysArray = Array.from(map.keys());
+
+    for (const key of keysArray) {
+        answer *= map.get(key) +1
+    }
+
+    return answer-1
+}
+
