466fd0e67ea0a6fcee400a08ce801f7ba1457d83
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-02-18 12:44:01 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1816 ms, Memory: 319060 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -0,0 +1,45 @@
+const fs=require("fs")
+let input=fs.readFileSync(0).toString().trim().split('\n')
+
+const [n,m]=input.shift().split(' ').map(Number)
+const map=input.map(str=>str.split('').map(Number))
+
+// index 0: 벽 부숨, index 1: 벽 안 부숨
+const visited = Array.from(Array(n), () => Array.from(Array(m), () => [1, 1]));
+const list=[[-1,0],[1,0],[0,1],[0,-1]]
+
+const bfs = () => {
+  let queue = [[0, 0, 0, 1]];
+  visited[0][0][1] = 0; // 시작점 방문 체크 (벽 안 부순 상태)
+  let index=0
+  while (index<queue.length) {
+    let [x, y, cnt, bool] = queue[index];
+    cnt++
+    index++
+
+    if (x === n - 1 && y === m - 1) return cnt;
+
+    for (const [dx, dy] of list) {
+      const nx = x + dx;
+      const ny = y + dy;
+
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+      if(nx===n-1&&ny===m-1){
+        return cnt+1
+      }
+      if (map[nx][ny] === 0 && visited[nx][ny][bool]) {
+        visited[nx][ny][bool] = 0;
+        queue.push([nx, ny, cnt, bool]);
+      } else if (bool && map[nx][ny] === 1 && visited[nx][ny][0]) {
+        // 벽이 있고, 아직 벽을 부순 적이 없는 경우
+        visited[nx][ny][0] = 0;
+        queue.push([nx, ny, cnt, 0]);
+      }
+    }
+  }
+
+  return -1;
+};
+
+
+console.log(bfs())
