681fa9bef0320c4fbd33a541f261c28755428dea
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-02-23 23:57:11 +0900

    [Silver I] Title: 안전 영역, Time: 372 ms, Memory: 18880 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input.shift());
+const map = input.map(str=>str.split(' ').map(Number));
+
+let max=0;
+const list=[[1,0],[-1,0],[0,1],[0,-1]]
+
+const bfs=(i,j,h)=>{
+  let queue=[[i,j]]
+  while(queue.length){
+    const [x,y]=queue.shift();
+    visited[x][y]=0;
+    
+    for(let [dx,dy] of list){
+      let cx=x+dx;
+      let cy=y+dy;
+      if(cx<0 || cx>=n || cy<0 ||cy>=n) continue;
+      if(visited[cx][cy] && map[cx][cy]>h){
+        queue.push([cx,cy])
+        visited[cx][cy]=0
+      }
+    }
+  }
+}
+
+for(let h=1; h<=100; h++){
+  let count=0;
+  var visited=Array.from(Array(n), ()=>Array(n).fill(1))
+  for(let i=0; i<n; i++){
+    for(let j=0; j<n; j++){
+      if(visited[i][j] && map[i][j]>h){
+        count++;
+        bfs(i,j,h)
+      }
+    }
+  }
+  max=Math.max(max, count)
+}
+
+console.log(max===0? 1: max)
+
