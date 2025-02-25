85c4934b65b99717d62fdb8e3e1efc10065f2c65
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-02-25 16:30:07 +0900

    [Silver I] Title: 기타 레슨, Time: 212 ms, Memory: 26084 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.js
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.js
@@ -5,21 +5,25 @@ const [n, m] = input[0].split(' ').map(Number);
 const list = input[1].split(' ').map(Number);
 
 let left=Math.max(...list);
-let right=list.reduce((a,b)=> a+b,0)
+let right=10000*m
 
 while(left<=right){
   let mid=~~((left+right)/2)
   
   let blueNum=1; // mid크기일 시 블루레이 개수
   let oneBlue=0; // 각 블루레이크기 확인 변수
+  
   for(const n of list){
+     // 현재 플루레이의 크기에 더해도 mid 이하일 때
     if(oneBlue+n <= mid) oneBlue +=n
     else{
+      // 새로 블루레이 크키를 현재 강의 시간으로 초기화
       oneBlue= n
-      blueNum++
+      blueNum++ // 블루레이 개수 추가
     }
   }
-  if(blueNum <=m) right=mid-1
+  
+  if(blueNum <= m) right=mid-1
   else left=mid+1
 }
 
