449352ce9551e10b8c1d270dc894ed91489d3a38
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-03-21 12:03:36 +0900

    [level 2] Title: 타겟 넘버, Time: 15.96 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
 
 ### 성능 요약
 
-메모리: 35.5 MB, 시간: 30.59 ms
+메모리: 35.4 MB, 시간: 15.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 20:53:48
+2025년 03월 21일 12:03:33
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,23 +1,17 @@
 function solution(numbers, target) {
-    let answer = 0;
-    const length=numbers.length;
+    let count=0;
+    let length=numbers.length;
     
-    const dfs = (idx,curX) =>{
-        if(idx == length && curX == target){
-            //numbers 내 모든 수를 돌았고, 타겟 넘버에 맞춤
-            answer += 1;
-            return
-        }
-        else if(idx == length && curX != target){
-            //numbers 내 모든 수를 돌았지만 타겟넘버에 맞추지 못함
+    dfs(0,-1);
+    function dfs(num, index){
+        if(index===length-1){
+            if(num===target) count++;
             return;
         }
-        //모든 수에 대해 +/- 재귀함수로 돌기
-        dfs(idx + 1, curX - numbers[idx]);
-        dfs(idx + 1, curX + numbers[idx]);
+        else{
+            dfs(num+numbers[index+1], index+1);
+            dfs(num-numbers[index+1], index+1);
+        }
     }
-
-    dfs(0,0); //재귀함수 시작
-
-    return answer;
+    return count;
 }
