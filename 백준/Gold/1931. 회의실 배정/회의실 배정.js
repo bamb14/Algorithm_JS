e7d05701f2359e5013a9ed9b4a603b860a9fd131
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-04-02 09:24:02 +0900

    [Gold V] Title: 회의실 배정, Time: 364 ms, Memory: 43828 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -0,0 +1,26 @@
+const fs=require("fs");
+let input=fs.readFileSync(0).toString().trim().split('\n');
+
+const n=Number(input.shift());
+const list=input.map(str=>str.split(' ').map(Number));
+list.sort((a,b)=>{
+  if(a[1]===b[1]) return a[0]-b[0]
+  else return a[1]-b[1]
+});
+
+let count=1;
+let endTime=list[0][1];
+
+for(let i=1; i<n; i++){
+  // for(let idx=i; idx<n; idx++){
+    const [start, end]=list[i];
+    if(start>=endTime){
+      count++;
+      // i=idx;
+      endTime=end;
+      // break;
+    }
+  // }
+}
+
+console.log(count);
