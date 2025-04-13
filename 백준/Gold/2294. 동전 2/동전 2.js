c068df7fad5df8e3c20314344983f1967b42296f
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-04-13 16:35:09 +0900

    [Gold V] Title: 동전 2, Time: 160 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.js
+++ 백준/Gold/2294. 동전 2/동전 2.js
@@ -5,12 +5,11 @@ const [n,k]=input.shift().split(' ').map(Number);
 const coin=input.map(Number).sort((a,b)=>a-b);
 
 const dp= new Array(k+1).fill(Infinity);
+dp[0]=0;
 
 for(let i=0; i<n; i++){
   for(let j=coin[i]; j<k+1; j++){
-    if(j%coin[i]===0) dp[j]=Math.min(dp[j],j/coin[i]);
-    else dp[j] = Math.min(dp[j], 1+dp[j - coin[i]]);
-  
+    dp[j] = Math.min(dp[j], 1+dp[j - coin[i]]);
   }
 }
 console.log(dp[k]===Infinity? -1: dp[k]);
