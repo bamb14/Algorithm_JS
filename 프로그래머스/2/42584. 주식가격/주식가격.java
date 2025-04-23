823010b4ac4bede90093c4f419cd243b3247cf7c
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-04-24 01:23:45 +0900

    [level 2] Title: 주식가격, Time: 13.75 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int len=prices.length;
+        int[] answer = new int[len];
+        
+        for(int i=0; i<len-1; i++){
+            int price=prices[i];
+            int sec=0;
+            for(int j=i+1; j<len; j++){
+                sec++;
+                if(price>prices[j]) break;
+            }
+            answer[i]=sec;
+        }
+        
+        return answer;
+    }
+}
