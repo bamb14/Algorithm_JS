05abb42bc0d42b05985d4ac19ef63b7b02b816c4
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-04-24 16:43:42 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -6,10 +6,10 @@ class Solution {
             map.put(comb[1], map.getOrDefault(comb[1],0)+1);
         }
         int answer=1;
-        for(Map.Entry<String,Integer> entry:map.entrySet()){
-            answer*=(entry.getValue()+1);
+        Iterator<Integer> iter = map.values().iterator();
+        while(iter.hasNext()){
+            answer*=(iter.next()+1);
         }
-
         return answer-1;
     }
 }
