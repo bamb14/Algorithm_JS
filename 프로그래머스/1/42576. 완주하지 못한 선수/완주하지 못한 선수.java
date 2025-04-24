a536a3dbbc8b60e87293d6f3d6968b88d654472c
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-04-24 15:38:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.63 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,9 +5,7 @@ class Solution {
         HashMap<String, Integer> map = new HashMap<>();
         
         for(String name : participant){
-            if(map.containsKey(name)){
-                map.put(name, map.get(name)+1);
-            }else map.put(name, 1);
+            map.put(name, map.getOrDefault(name,0)+1);
         }
         
         for(String name : completion){
