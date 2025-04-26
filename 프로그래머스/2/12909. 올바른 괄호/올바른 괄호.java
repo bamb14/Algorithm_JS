64770ef08521a1ee1a802b74e94d4ae14ac1587c
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-04-26 17:29:01 +0900

    [level 2] Title: 올바른 괄호, Time: 21.36 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,14 +2,14 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         Stack<Character> stack=new Stack<>();
-        char[] arr = s.toCharArray();
+        // String[] arr = s.split("");
         
-        for(char str:arr){
-            if(str==')'){
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==')'){
                 if(!stack.isEmpty()&&stack.peek()=='(') stack.pop();
-                else stack.push(str);
+                else stack.push(s.charAt(i));
             }
-            else stack.push(str);
+            else stack.push(s.charAt(i));
         }
 
         return stack.size()>0? false: true;
