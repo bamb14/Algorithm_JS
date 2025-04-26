8bf673b485c2033ff2ed5ed67a769942047523a2
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-04-26 17:53:04 +0900

    [level 2] Title: 프로세스, Time: 1.69 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,26 +2,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         Queue<Integer> queue=new LinkedList<>();
-        List<int[]> pq = new ArrayList<>();
+        PriorityQueue<Integer> priority=new PriorityQueue<>((a,b)->b-a);
         for(int i=0; i<priorities.length; i++){
             queue.offer(i);
-            pq.add(new int[]{priorities[i], i});
+            priority.offer(priorities[i]);
         }
-        pq.sort((a,b)->b[0]-a[0]);
-        // System.out.println(pq);
-        int answer = 0;
-        int index=0;
+        int answer=0;
         while(!queue.isEmpty()){
-            int curr = queue.poll();
-
-            if (priorities[curr] < pq.get(index)[0]) {
-                queue.offer(curr); // 우선순위가 낮으면 다시 뒤로
-            } else {
+            int curr=queue.poll();
+            if(priorities[curr]<priority.peek()) queue.offer(curr);
+            else{
+                priority.poll();
                 answer++;
-                if (curr == location) break;
-                index++; // 다음 높은 우선순위로
+                if(curr==location) return answer;
             }
         }
-        return answer;
+        
+        return -1;
     }
 }
