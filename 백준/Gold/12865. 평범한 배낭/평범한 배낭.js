3b0adbd8157a83da93c17ff8f10ecffde7239e62
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-05-17 17:39:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 368 ms, Memory: 90432 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -1,15 +1,26 @@
-const fs = require("fs");
-let a = fs.readFileSync(0).toString().trim().split('\n');
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [N, K] = a.shift().split(' ').map(Number);
-const dp = Array.from({ length: N + 1 }, () => Array(K + 1).fill(0));
+let [n,k] = input.shift().split(' ').map(Number);
+let list=input.map(str=>str.split(' ').map(Number)).sort((a,b)=>a[0]-b[0]);
 
-for (let i = 1; i < N + 1; i++) {
-  const [W, V] = a[i-1].split(' ').map(Number);;
-  for (let j = 1; j <= K; j++) {
-    if (j - W >= 0) dp[i][j] = Math.max(dp[i - 1][j - W] + V, dp[i - 1][j]);
-    else dp[i][j] = dp[i - 1][j];
+let dp=Array.from(Array(n), ()=>Array(k+1).fill(0));
+// dp[i][j]: i번째 물건을 넣거나 넣지 않았을때
+// 무게 k에 담을 수 있는 최대 가치
+
+for(let j=1; j<=k; j++){
+  if(list[0][0]>j) continue;
+  else dp[0][j]=list[0][1];
+}
+let answer=0;
+
+for(let i=1; i<n; i++){
+  let [w, v]=list[i];
+  for(let j=1; j<=k; j++){
+    if(w>j) dp[i][j]=dp[i-1][j];
+    else {
+      dp[i][j]=Math.max(dp[i-1][j], dp[i-1][j-w]+v);
+    }
   }
 }
-
-console.log(dp[N][K]);
+console.log(dp[n-1][k]);
