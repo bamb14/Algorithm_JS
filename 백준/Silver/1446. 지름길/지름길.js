15314cb5c38ee6f117cff735066ed22f1510cd61
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-06-19 16:55:46 +0900

    [Silver I] Title: 지름길, Time: 92 ms, Memory: 9404 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.js
+++ 백준/Silver/1446. 지름길/지름길.js
@@ -1,20 +1,23 @@
-const fs=require("fs")
-let input=fs.readFileSync(0).toString().trim().split('\n')
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [n,d]=input.shift().split(' ').map(Number)
-const list=input.map(str=>str.split(' ').map(Number))
+let [n,d] = input.shift().split(' ').map(Number);
+let info=input.map(str=>str.split(' ').map(Number));
+info.sort((a,b)=>a[1]-b[1]);
 
-let dp=new Array(d+1).fill(Infinity)
-dp[0]=0
+let dp=new Array(d+1).fill(Infinity);
+dp[0]=0;
+let index=0;
 
-for (let i = 0; i <= d; i++) {
-  if (i > 0) dp[i] = Math.min(dp[i], dp[i - 1] + 1);
+for(let i=1; i<=d; i++){
+  if(index<n && info[index][1]<=i){
+    while(index<n && info[index][1]<=i){
+      dp[i]=Math.min(Math.min(dp[i-1]+1,dp[i]), dp[info[index][0]] + info[index][2]);
 
-  for (const [start, end, dist] of list) {
-    if (start === i && end <= d) {
-      dp[end] = Math.min(dp[end], dp[start] + dist);
+      index++;
     }
   }
+  else dp[i]=dp[i-1]+1;
 }
 
-console.log(dp[d])
+console.log(dp[d]);
