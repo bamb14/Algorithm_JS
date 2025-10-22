a745252fde8105294da4c54684bedbdb1edfd1b8
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-10-22 17:02:53 +0900

    [Gold IV] Title: 부분합, Time: 176 ms, Memory: 16640 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.js
+++ 백준/Gold/1806. 부분합/부분합.js
@@ -1,23 +1,19 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split("\n");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [n, s] = input[0].split(" ").map(Number);
-const list = input[1].split(" ").map(Number);
+const [n,s]=input[0].split(' ').map(Number);
+const list=input[1].split(' ').map(Number);
 
-let left = 0, right = 0;
-let sum = 0;
-let answer = Infinity;
+let left=0, right=0;
+let sum=list[left];
+let len=Infinity;
 
-while (right < n) {
-    sum += list[right];
-    
-    while (sum >= s) {
-        answer = Math.min(answer, right - left + 1);
-        sum -= list[left];
-        left++;
-    }
-
-    right++;
+while(right<n && left<=right){
+  if(sum>=s){
+    len=Math.min(len, right-left+1);
+    sum-=list[left++];
+  }
+  else sum+=list[++right];
 }
 
-console.log(answer === Infinity ? 0 : answer);
+console.log(len===Infinity? 0: len);
