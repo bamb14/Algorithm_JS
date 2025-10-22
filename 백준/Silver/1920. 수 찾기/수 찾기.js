aefbf3ffd96a07e6b41000817c0d84e5c94b38c8
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-10-22 14:24:06 +0900

    [Silver IV] Title: 수 찾기, Time: 272 ms, Memory: 49800 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -2,26 +2,17 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const N=Number(input[0]);
-const list=input[1].split(' ').map(Number).sort((a,b)=>a-b);
+const list=input[1].split(' ').map(Number);
 const M=Number(input[2]);
 const numbers=input[3].split(' ').map(Number);
 
+const set=new Set();
+for(const n of list) set.add(n);
+
 const answer=[];
 for(const n of numbers){
-  let left=0, right=N-1;
-  let flag=0;
-  while(left<=right){
-    const mid=Math.floor((left+right)/2);
-    
-    if(list[mid]===n){
-      flag=1;
-      break;
-    }
-    if(list[mid]<=n) left=mid+1;
-    else right=mid-1;
-  }
-  
-  answer.push(flag);
+  if(set.has(n)) answer.push(1);
+  else answer.push(0);
 }
 
 console.log(answer.join('\n'));
