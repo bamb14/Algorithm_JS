73a105a292d17f56b4c1a0275ef7c2987a0d7922
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-11-19 02:02:39 +0900

    [Silver I] Title: 단축키 지정, Time: 92 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.js
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.js
@@ -1,46 +1,33 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split('\n');
+const input = fs.readFileSync(0).toString().trim().split("\n");
 
-let n = Number(input.shift());
-let list=input;
+const n=Number(input.shift());
+
 const set=new Set();
-const result=[];
+const answer=[];
 
-for(const word of list){
-  let i=0;
-  let plag=false;
-  let prev=[];
-  let words=word.split(' ');
-  while(i<words.length){
-    if(!set.has(words[i][0].toUpperCase())){
-      set.add(words[i][0].toUpperCase());
-      let curr='['+words[i][0]+']'+words[i].slice(1,);
-      let next=[];
-      for(let j=i+1; j<words.length; j++){
-        next.push(words[j]);
-      }
-
-      prev.push(curr);
-      prev.push(...next);
-      result.push(prev.join(' '));
-      
-      plag=true;
+for(let word of input){
+  const list=word.split(' ');
+  let flag=false;
+  for(let i=0; i<list.length; i++){
+    if(!set.has(list[i][0].toUpperCase())){
+      set.add(list[i][0].toUpperCase());
+      list[i]='['+list[i][0]+']'+list[i].slice(1,);
+      flag=true;
+      answer.push(list.join(' '));
       break;
     }
-    prev.push(words[i]);
-    i++;
   }
-  if(!plag){
+  if(!flag){
     for(let i=1; i<word.length; i++){
       if(word[i]!==' ' && !set.has(word[i].toUpperCase())){
         set.add(word[i].toUpperCase());
-        result.push(word.slice(0,i)+'['+word[i]+']'+word.slice(i+1,));
-        plag=true;
+        word=word.slice(0,i)+'['+word[i]+']'+word.slice(i+1,);
         break;
       }
     }
+    answer.push(word);
   }
-  if(!plag) result.push(word);
 }
 
-console.log(result.join('\n'));
+console.log(answer.join('\n'));
