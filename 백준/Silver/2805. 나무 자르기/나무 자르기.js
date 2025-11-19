0b756ce5912d8c6b7595f1eb102a8bf8b9db85e0
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-11-19 20:07:17 +0900

    [Silver II] Title: 나무 자르기, Time: 636 ms, Memory: 139496 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
+const input = fs.readFileSync(0).toString().trim().split("\n");
 
-const [n, m]=input[0].split(' ').map(Number);
+const [n,m]=input[0].split(' ').map(Number);
 const list=input[1].split(' ').map(Number);
 
+let left=0; right=Math.max(...list);
 let answer=0;
-let left=0, right=Math.max(...list);
 
 while(left<=right){
   const mid=Math.floor((left+right)/2);
-  
-  let sum=0;
+
+  let num=0;
   for(const h of list){
-    if(h>mid) sum+=h-mid;
+    if(h>mid) num+=h-mid;
   }
-  
-  if(sum>=m){
-    left=mid+1;
+
+  if(num>=m){
     answer=Math.max(answer, mid);
+    left=mid+1;
   }
   else right=mid-1;
 }
