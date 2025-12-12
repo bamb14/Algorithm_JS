7f4de6eaa6fc7212554ddc419955d534bbc9c137
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-12-12 21:55:53 +0900

    Simplify tuple parsing in solution function

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,26 +1,7 @@
 function solution(s) {
-    s=s.slice(1,s.length-1);
-    const list=[];
-    for(let i=0; i<s.length; i++){
-        if(s[i]==='{'){
-            i++;
-            const arr=[];
-            let num='';
-            while(s[i]!=='}'){
-                if(s[i]!==',') num+=s[i];
-                else{
-                    arr.push(Number(num));
-                    num='';
-                }
-                i++;
-            }
-            if(num!=='') arr.push(Number(num));
-            list.push(arr);
-        }
-    }
-    const answer=[];
-    
+    const list=s.slice(2,-2).split('},{').map(str=>str.split(',').map(Number));
     list.sort((a,b)=>a.length-b.length);
+    const answer=[];
     const set=new Set();
     for(const item of list){
         for(const n of item){
