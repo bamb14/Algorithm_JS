5518da95869ad8d53074a659a03f3470749235a9
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-12-13 15:40:29 +0900

    [Gold V] Title: 치킨 배달, Time: 192 ms, Memory: 12604 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.js
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.js
@@ -1,46 +1,65 @@
 const fs=require("fs");
-let input=fs.readFileSync(0).toString().trim().split('\n');
+const input=fs.readFileSync(0).toString().trim().split('\n');
 
 const [n,m]=input.shift().split(' ').map(Number);
-const graph=input.map(str=>str.split(' ').map(Number));
-const list=[[-1,0],[1,0],[0,1],[0,-1]];
-const homes=[];
-const chickenList=[];
-
+const map=input.map(str=>str.split(' ').map(Number));
+const move=[[0,1],[0,-1],[1,0],[-1,0]];
+const chickens=[];
+const houses=[];
 for(let i=0; i<n; i++){
   for(let j=0; j<n; j++){
-    if(graph[i][j]===1) homes.push([i,j]);
-    else if(graph[i][j]===2) chickenList.push([i,j]);
+    if(map[i][j]==2){
+      chickens.push([i,j]);
+    }
+    if(map[i][j]==1){
+      houses.push([i,j]);
+    }
   }
 }
-const visited= new Array(chickenList.length).fill(0);
-let answer=[];
-let min=Infinity;
 
-backtracking(0,0);
-console.log(min);
+const result=[];
 
-function backtracking(depth, idx){
-  if(depth===m){
-      let total=0;
-      for(const [hx,hy] of homes){
-        let dist=Infinity;
-        for(const [cx,cy] of answer){
-          dist=Math.min(dist, Math.abs(hx-cx)+Math.abs(hy-cy));
-        }
-        total+=dist
+bt([], 0);
+
+console.log(result.sort((a,b)=>a-b)[0]);
+
+function bfs(i, j, arr){
+  const queue=[[i,j,0]];
+  const visited=Array.from(Array(n), ()=>Array(n).fill(false));
+  visited[i][j]=true;
+  while(queue.length>0){
+    const [x,y,cnt]=queue.shift();
+    
+    for(const [dx,dy] of move){
+      let cx=x+dx, cy=y+dy;
+      if(cx<0 || cy<0 || cx>=n || cy>=n) continue;
+      
+      if(!visited[cx][cy]){
+        if(map[cx][cy]==2 && arr.some(([a,b])=>a==cx&&b==cy))return cnt+1;
+        visited[cx][cy]=true;
+        queue.push([cx,cy,cnt+1]);
       }
-      min=Math.min(min, total);
-      return;
+    }
   }
-  
-  for(let i=idx; i<chickenList.length; i++){
-      if(!visited[i]){
-          visited[i]=1;
-          answer.push(chickenList[i])
-          backtracking(depth+1, i);
-          answer.pop();
-          visited[i]=0;
+}
+
+function bt(arr, idx){
+  if(arr.length>=m){
+    let total=0;
+    for(const [x,y] of houses){
+      let dist=Infinity;
+      for(let i=0; i<arr.length; i++){
+        const [cx,cy]=chickens[arr[i]];
+        dist=Math.min(dist, Math.abs(x-cx)+Math.abs(y-cy));
       }
+      total+=dist;
+    }
+    result.push(total);
+    return;
+  }
+  for(let i=idx; i<chickens.length; i++){
+    arr.push(i);
+    bt(arr, i+1);
+    arr.pop();
   }
 }
