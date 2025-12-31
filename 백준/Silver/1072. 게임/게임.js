9d48a88fab14b54d0f97ebea4a3ffc26f4af14b3
Miso Kim <118671229+bamb14@users.noreply.github.com>
2025-12-31 21:51:12 +0900

    [Silver III] Title: 게임, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.js
+++ 백준/Silver/1072. 게임/게임.js
@@ -8,18 +8,15 @@ if(z>=99) console.log(-1);
 else{
   let left=1, right=x;
   
-  let answer=x;
-  
   while(left<=right){
     const mid=Math.floor((left+right)/2);
     
     let newZ=Math.floor((y+mid)*100/(x+mid));
     if(z<newZ){
       right=mid-1;
-      answer=Math.min(answer, mid);
     }
     else left=mid+1;
   }
   
-  console.log(answer);
+  console.log(left);
 }
