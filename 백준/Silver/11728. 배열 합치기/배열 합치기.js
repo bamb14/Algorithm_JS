48266e0e326ea03d39314bc94f0d8c279023412d
Miso Kim <118671229+bamb14@users.noreply.github.com>
2026-01-02 00:08:15 +0900

    [Silver V] Title: 배열 합치기, Time: 1472 ms, Memory: 368472 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.js
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.js
@@ -5,36 +5,23 @@ const [n,m]=input.shift().split(' ').map(Number);
 const arr1=input[0].split(' ').map(Number);
 const arr2=input[1].split(' ').map(Number);
 
-const answer=[];
-
-let p1=0, p2=0;
+const answer=new Array(n+m);
+let i=0, p1=0, p2=0;
 
 while(p1<n && p2<m){
   const n1=arr1[p1];
   const n2=arr2[p2];
-  if(n1==n2){
-    answer.push(n1);
-    answer.push(n2);
-    p1++;
-    p2++;
-  }
-  if(n1<n2){
-    answer.push(n1);
+  if(n1<=n2){
+    answer[i++]=n1;
     p1++;
   }
-  else if(n1>n2){
-    answer.push(n2);
+  else {
+    answer[i++]=n2;
     p2++;
   }
 }
 
-if(p1===n){
-  const remain=arr2.slice(p2,);
-  answer.push(...remain);
-}
-else {
-  const remain=arr1.slice(p1,);
-  answer.push(...remain);
-}
+while(p1<n) answer[i++]=arr1[p1++];
+while(p2<m) answer[i++]=arr2[p2++];
 
 console.log(answer.join(' '));
