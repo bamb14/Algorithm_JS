c825d755a497b35cc10dc64889860019998b5576
Miso Kim <118671229+bamb14@users.noreply.github.com>
2026-01-01 21:08:50 +0900

    [Silver IV] Title: 주몽, Time: 160 ms, Memory: 12608 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.js
+++ 백준/Silver/1940. 주몽/주몽.js
@@ -4,22 +4,21 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const n=Number(input.shift());
 const m=Number(input.shift());
 const list=input[0].split(' ').map(Number);
+list.sort((a,b)=>a-b);
 
-const visited=new Array(n).fill(false);
 let answer=0;
+let left=0, right=n-1;
 
-for(let i=0; i<n-1; i++){
-  if(visited[i]) continue;
-  for(let j=i+1; j<n; j++){
-    if(visited[j]) continue;
-    
-    if(list[i]+list[j]==m){
-      visited[i]=true;
-      visited[j]=true;
-      answer++;
-      break;
-    }
+while(left<right){
+  const sum=list[left]+list[right];
+  
+  if(sum==m){
+    answer++;
+    left++;
+    right--;
   }
+  if(sum<m) left++;
+  if(sum>m) right--;
 }
 
 console.log(answer);
