44f238bda353d6fdd5c987db407022648e8cc821
Miso Kim <118671229+bamb14@users.noreply.github.com>
2026-03-19 22:14:16 +0900

    [Gold I] Title: K번째 수, Time: 168 ms, Memory: 10380 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n=Number(input[0]);
+const k=Number(input[1]);
+
+let left=1, right=k;
+
+while(left<=right){
+  let mid=Math.floor((left+right)/2);
+  let cnt=0;
+  
+  for(let i=1; i<=n; i++){
+    cnt+=Math.min(Math.floor(mid/i), n);
+  }
+  
+  if(cnt<k) left=mid+1;
+  else right=mid-1;
+}
+
+console.log(left);
