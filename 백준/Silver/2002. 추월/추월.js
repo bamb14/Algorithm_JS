5e771f88832a0ff1a1fecbec6c3b1e2f9aa1461c
Miso Kim <118671229+bamb14@users.noreply.github.com>
2026-04-05 23:05:27 +0900

    [Silver I] Title: 추월, Time: 92 ms, Memory: 9460 KB -BaekjoonHub

---
+++ 백준/Silver/2002. 추월/추월.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n=Number(input.shift());
+const before=input.slice(0, n);
+const after=input.slice(n,);
+const visited=new Set();
+
+let curr=0;
+let answer=0;
+for(let i=0; i<n; i++){
+  while(curr < n && visited.has(before[curr])) curr++;
+  
+  if(before[curr]==after[i]) curr++;
+  else{
+    if(before[curr]==after[i]) curr++;
+    else{
+      visited.add(after[i]);
+      answer++;
+    }
+  }
+}
+
+console.log(answer);
