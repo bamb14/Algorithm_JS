4b97a5f2951faa15084bf12220f0b2f99b99ad64
Miso Kim <118671229+bamb14@users.noreply.github.com>
2026-08-30 15:11:26 +0900

    [level 2] Title: 가장 큰 수, Time: 1543.87 ms, Memory: 394 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,19 +1,17 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        PriorityQueue<String> pq= new PriorityQueue<>(
-            (a, b) -> (b + a).compareTo(a + b) // 큰 숫자가 먼저 오도록 내림차순 정렬
-        );
-        for(int n : numbers){
-            pq.add(Integer.toString(n));
-        }
-        // System.out.print(pq);
-        while (!pq.isEmpty()) {
-            answer+=pq.poll();
-        }
-        if(answer.charAt(0)=='0') return "0";
-
+        String[] arr = new String[numbers.length];
+        
+        for(int i=0; i<numbers.length; i++) arr[i]=String.valueOf(numbers[i]);
+        
+        Arrays.sort(arr, (a,b)-> (b+a).compareTo(a+b));   
+        
+        if(arr[0].equals("0")) return "0";
+        
+        String answer="";
+        for(String str : arr) answer+=str;
+        
         return answer;
     }
 }
